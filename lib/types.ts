2888dbc0005730ea033fa72db5d3d73b44a520f7
hornmush <tgon1003@gmail.com>
2026-07-27 01:40:07 +0900

    fix alarm 4

--- lib/types.ts
+++ lib/types.ts
@@ -1,17 +1,13 @@
-export type Room = {
-  id: string;
-  slug: string;
-  created_at: string;
-};
-
 export type Post = {
   id: string;
   room_id: string;
   parent_id: string | null;
   nickname: string;
   product_name: string;
-  barcode?: string | null;
+  barcode: string | null;
   content: string;
+  password?: string;
+  completed: boolean; // 이 부분이 추가되었습니다!
   created_at: string;
 };
 
